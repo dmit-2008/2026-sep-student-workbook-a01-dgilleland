68f08e2a812efe0f3e6f15a11787b4953f9e17a9
Dan Gilleland <dgilleland@nait.ca>
2026-09-04 11:44:40 -0600

    First JS Refresher

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -6,3 +6,30 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+console.log('index.js has loaded');
+
+const handleSubmit = function(evt) {
+    // client-side only
+    evt.preventDefault(); // stop the POST to the webserver
+
+    const targetForm = evt.target; // should be the <form> element
+    const textInput = targetForm.elements['new-topic']; // <input />
+    console.log(textInput.value);
+
+    if(textInput.value.trim() !== '') {
+        // Good, I have text... 🚀
+        const list = document.querySelector('ul');
+        // v1 - just "quick-n-dirty" approach 🐞
+        list.innerHTML += `<li>${textInput.value.trim()}</li>`;
+
+        textInput.value = ''; // clear the input
+        textInput.classList.remove('is-invalid');
+    } else {
+        // Bad input 😢
+        textInput.classList.add('is-invalid');
+    }
+    textInput.focus(); // drive keyboard focus to the input
+}
+
+const myForm = document.querySelector('form');
+myForm.addEventListener('submit', handleSubmit);
